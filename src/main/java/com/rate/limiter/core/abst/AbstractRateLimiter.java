ad91d3698d28f00a277bfaccfb6bfc915ad57313
Mayank Kumar <mayankk.co@nsit.net.in>
2022-08-11 23:40:24 +0530

    object not found error added

--- src/main/java/com/rate/limiter/core/abst/AbstractRateLimiter.java
+++ src/main/java/com/rate/limiter/core/abst/AbstractRateLimiter.java
@@ -1,8 +1,11 @@
 package com.rate.limiter.core.abst;
 
+import com.rate.limiter.core.errors.ObjectNotFoundError;
 import com.rate.limiter.core.factory.JedisFactory;
 import com.rate.limiter.core.inter.JedisService;
 import com.rate.limiter.model.dto.Configuration;
+
+import java.util.Objects;
 
 public abstract class AbstractRateLimiter {
    protected abstract Boolean isLimitLeft(String id);
@@ -16,6 +19,13 @@ public abstract class AbstractRateLimiter {
    protected AbstractRateLimiter(Configuration configuration) {
       this.jedisService = JedisFactory.getJedisService(configuration.getRedisHost(),
               configuration.getRedisPort(), configuration.getRedisDb());
+   }
+
+   protected JedisService getJedisService() {
+      if (Objects.isNull(jedisService)) {
+         throw new ObjectNotFoundError(JedisService.class.getName());
+      }
+      return jedisService;
    }
 
    public Boolean check(String id) {
--- src/main/java/com/rate/limiter/utils/ErrorConstants.java
+++ src/main/java/com/rate/limiter/utils/ErrorConstants.java
@@ -2,4 +2,5 @@ package com.rate.limiter.utils;
 
 public interface ErrorConstants {
     String WRONG_TYPE_ERROR = "wrong type provided to factory";
+    String OBJECT_NOT_FOUND_ERROR = "object not found error";
 }
